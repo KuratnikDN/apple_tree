bff77d7991a6e92bdd5e98c3337b3f9bfd856ff5
vildhet <vildhet@gmail.com>
2013-09-09 22:58:25 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: werewolf
+ *
+ * Created on September 9, 2013, 10:56 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
